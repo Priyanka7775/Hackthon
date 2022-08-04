67003b4424ada4e6be90766b78a77a009cb021cc
839168_Priyanka <priyankadewangan7775@gmail.com>
2022-08-04 12:07:11 +0000

    Auto saving changes from StackRoute workbench on Thu Aug 04 2022 12:07:05 GMT+0000 (Coordinated Universal Time)

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -14,38 +14,42 @@ public class SortingCities
 	 s.greaterThan(city,distance);
 	}
 
-	public  void convetUpperCase(String[] city){
+	public String convetUpperCase(String[] city){
 	  for(int i=0;i<city.length;i++){	
 		System.out.print(city[i].toUpperCase()+" ");
 		}
+		return city[i].toUpperCase;
 		System.out.println();
 	 }
 	
-	public void printCityAndDistance(String[] city, int[] distance){
-		
+	public String printCityAndDistance(String[] city, int[] distance){
+		String[] cityAndDistance=new String[city.length];
 		for(int i=0;i<city.length;i++){
 	
 			System.out.println(city[i]+":"+distance[i]+"kms");
 		}
+		return cityAndDistance;
 		System.out.println();
 	}
-	public void findCityDistance(String[] city, int[] distance){
+	public  String findCityDistance(String[] city, int[] distance){
 		
 		for(int i=0;i<distance.length;i++){
 			if(distance[i]==52){
 			System.out.println("The city which is 52kms from Zurish"+city[i]);
 			}
-				System.out.println();	
+			return;
+			System.out.println();	
 		}
 		
 		
 	}
-	public void greaterThan(String[] city, int[] distance){
+	public  String greaterThan(String[] city, int[] distance){
 		
 			for(int i=0;i<distance.length;i++){
 				if(distance[i]>=270){
 				System.out.println("The city which is greater than or equal to 270km from Zurish is "+city[i]);
 				}
+					return;
 					System.out.println();	
 			}
 		}
