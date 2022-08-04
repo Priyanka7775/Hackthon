085b840088a9f38bb4bd0e1607de65354932271d
839168_Priyanka <priyankadewangan7775@gmail.com>
2022-08-04 10:27:20 +0000

    Saving changes from StackRoute workbench on Thu Aug 04 2022 15:57:06 GMT+0530 (India Standard Time)

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -5,7 +5,7 @@ public class SortingCities
 {
     public static void main( String[] args ) {
      String [] city={"Bern","Lucerne","Interlaken","Grinedland","Engelberg","Geneva","Murren",	"Basel","Zermatt","Jungfraujoch"};
-	String [] distance={"138","52","118","136","85","276","103","87","214","101"};
+	int [] distance={138,52,118,136,85,276,103,87,214,101};
 
 	 SortingCities s= new SortingCities();
 	 s.convetUpperCase(city);
@@ -21,7 +21,7 @@ public class SortingCities
 		System.out.println();
 	 }
 	
-	public void printCityAndDistance(String[] city, String[] distance){
+	public void printCityAndDistance(String[] city, int[] distance){
 		String[] CityAndDistance=new String[city.length];
 		for(int i=0;i<city.length;i++){
 	
@@ -29,13 +29,14 @@ public class SortingCities
 		}
 		System.out.println();
 	}
-	public void findCityDistance(String[] city, String[] distance){
+	public void findCityDistance(String[] city, int[] distance){
 		String[] findCity=new String[city.length];
-		if(distance.equals(52)){
-			for(int i=0;i<city.length;i++){
-				System.out.println("The city which is 52kms from Zurich"+city[i]);
-			}
+		for(int i=0;i<distance.length;i++){
+			if(distance[i]==52){
+			System.out.println(city[i]);
 		}
+		
+		
 	}
 	public void greaterThan(String[] city, String[] distance){
 		String[] greater=new String[distance.length];
